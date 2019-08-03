eddc7b9c21913f405c0f9c57a54b97a42d1d757d
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-02 23:43:35 -0400

    Added a new problem & solution.

---
+++ 2019/08_01_2019/reversed_strings.rb
@@ -0,0 +1,38 @@
+# Date:  8/01/19 remote meeting
+# Name: Reversed Strings
+# Difficulty: 8 kyu
+
+# --- sources ---
+# Website:
+# https://www.codewars.com/kata/reversed-strings/train/ruby
+
+# Provided by:
+# Adeel
+
+# Attempted by:
+# Kazumi
+
+##################
+#                #
+#  Instructions  #
+#                #
+##################
+#
+# Complete the solution so that it reverses the string value
+# passed into it.
+#
+# solution('world') # returns 'dlrow'
+#
+##############
+# PSEUDOCODE #
+##############
+
+# None
+
+##################
+# RUBY SOLUTIONS #
+##################
+
+def solution(str)
+  str.reverse()
+end
